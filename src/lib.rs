08f077a13f3dce815ee52b0b01d791346e9252de
SiDRRR <69087150+SiDRRR@users.noreply.github.com>
2020-08-09 12:32:44 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
 use wasm_bindgen::prelude::*;
-
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn generate_name_str(seed: i32) -> String {
+    // the seed is coming from the JS side
+    let a = seed % (ADJECTIVES.len() as i32);
+    let b = seed % (NOUNS.len() as i32);
+    [ADJECTIVES[a as usize].to_string(), " ".to_string(), NOUNS[b as usize].to_string()].join("")
+}
+
+const ADJECTIVES: [&str; 6] = [
+"Rich",
+"Poor",
+"Cool",
+"Casual",
+"Genuine",
+"Good"
+];
+
+const NOUNS: [&str; 4] = [
+"Bob",
+"Naman",
+"Simon",
+"Joshua"
+];
+
+// debugging
+pub fn main() {
+  println!("{:?}", generate_name_str(1));
 }
